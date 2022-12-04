d680b60318be1a5add75c0e8932415942a082687
DallogFheir <dallog.fheir@gmail.com>
2022-12-04 12:25:37 +0100

    day 4

---
+++ src/day4/Callback.java
@@ -0,0 +1,5 @@
+package day4;
+
+public interface Callback {
+    public boolean call(SectionRange range1, SectionRange range2);
+}
---
+++ src/day4/SectionRange.java
@@ -0,0 +1,28 @@
+package day4;
+
+public final class SectionRange {
+    private int start;
+    private int end;
+
+    public SectionRange(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean contains(SectionRange other) {
+        return this.start <= other.start && this.end >= other.end;
+    }
+
+    public boolean overlapsWith(SectionRange other) {
+        return (this.end >= other.start && this.start <= other.end)
+                || (other.end >= this.start && other.start <= this.end);
+    }
+
+    public static SectionRange parseFromString(String str) {
+        String[] parts = str.split("-");
+        final int start = Integer.parseInt(parts[0]);
+        final int end = Integer.parseInt(parts[1]);
+
+        return new SectionRange(start, end);
+    }
+}
